82a778d2810178a5700826d202e3291e8bc3a99b
Max Twedt <mhtwedt@gmail.com>
2023-11-20 23:27:53 -0600

    fixing imgui branch

--- Monstera/src/Monstera/ImGui/ImGuiLayer.cpp
+++ Monstera/src/Monstera/ImGui/ImGuiLayer.cpp
@@ -3,8 +3,9 @@
 #include "ImGuiLayer.h"
 
 #include "imgui.h"
-#include "Platform/OpenGL/ImGuiOpenGL.h"
-//#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
 #include "Monstera/Application.h"
 
 // TEMPORARY
@@ -35,99 +36,18 @@ namespace Monstera {
 
 		// io.MousePos = ImVec2(0.0f, 0.0f);
 
-		//Temporary: will use monstera key codes
-
-		io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-		io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-		io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-		io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-		io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-		io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-		io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-		io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-		io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-		io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
-		io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-		io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-		io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
-		io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-		io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-		io.KeyMap[ImGuiKey_Apostrophe] = GLFW_KEY_APOSTROPHE;
-		io.KeyMap[ImGuiKey_Comma] = GLFW_KEY_COMMA;
-		io.KeyMap[ImGuiKey_Minus] = GLFW_KEY_MINUS;
-		io.KeyMap[ImGuiKey_Period] = GLFW_KEY_PERIOD;
-		io.KeyMap[ImGuiKey_Slash] = GLFW_KEY_SLASH;
-		io.KeyMap[ImGuiKey_Semicolon] = GLFW_KEY_SEMICOLON;
-		io.KeyMap[ImGuiKey_Equal] = GLFW_KEY_EQUAL;
-		io.KeyMap[ImGuiKey_LeftBracket] = GLFW_KEY_LEFT_BRACKET;
-		io.KeyMap[ImGuiKey_Backslash] = GLFW_KEY_BACKSLASH;
-		io.KeyMap[ImGuiKey_RightBracket] = GLFW_KEY_RIGHT_BRACKET;
-		io.KeyMap[ImGuiKey_GraveAccent] = GLFW_KEY_GRAVE_ACCENT;
-		io.KeyMap[ImGuiKey_CapsLock] = GLFW_KEY_CAPS_LOCK;
-		io.KeyMap[ImGuiKey_ScrollLock] = GLFW_KEY_SCROLL_LOCK;
-		io.KeyMap[ImGuiKey_NumLock] = GLFW_KEY_NUM_LOCK;
-		io.KeyMap[ImGuiKey_PrintScreen] = GLFW_KEY_PRINT_SCREEN;
-		io.KeyMap[ImGuiKey_Pause] = GLFW_KEY_PAUSE;
-		io.KeyMap[ImGuiKey_KeypadDecimal] = GLFW_KEY_KP_DECIMAL;
-		io.KeyMap[ImGuiKey_KeypadDivide] = GLFW_KEY_KP_DIVIDE;
-		io.KeyMap[ImGuiKey_KeypadMultiply] = GLFW_KEY_KP_MULTIPLY;
-		io.KeyMap[ImGuiKey_KeypadSubtract] = GLFW_KEY_KP_SUBTRACT;
-		io.KeyMap[ImGuiKey_KeypadAdd] = GLFW_KEY_KP_ADD;
-		io.KeyMap[ImGuiKey_KeypadEnter] = GLFW_KEY_KP_ENTER;
-		io.KeyMap[ImGuiKey_KeypadEqual] = GLFW_KEY_KP_EQUAL;
-		io.KeyMap[ImGuiKey_LeftShift] = GLFW_KEY_LEFT_SHIFT;
-		io.KeyMap[ImGuiKey_LeftCtrl] = GLFW_KEY_LEFT_CONTROL;
-		io.KeyMap[ImGuiKey_LeftAlt] = GLFW_KEY_LEFT_ALT;
-		io.KeyMap[ImGuiKey_LeftSuper] = GLFW_KEY_LEFT_SUPER;
-		io.KeyMap[ImGuiKey_RightShift] = GLFW_KEY_RIGHT_SHIFT;
-		io.KeyMap[ImGuiKey_RightCtrl] = GLFW_KEY_RIGHT_CONTROL;
-		io.KeyMap[ImGuiKey_RightAlt] = GLFW_KEY_RIGHT_ALT;
-		io.KeyMap[ImGuiKey_RightSuper] = GLFW_KEY_RIGHT_SUPER;
-		io.KeyMap[ImGuiKey_Menu] = GLFW_KEY_MENU;
-		io.KeyMap[ImGuiKey_0] = GLFW_KEY_0;
-		io.KeyMap[ImGuiKey_1] = GLFW_KEY_1;
-		io.KeyMap[ImGuiKey_2] = GLFW_KEY_2;
-		io.KeyMap[ImGuiKey_3] = GLFW_KEY_3;
-		io.KeyMap[ImGuiKey_4] = GLFW_KEY_4;
-		io.KeyMap[ImGuiKey_5] = GLFW_KEY_5;
-		io.KeyMap[ImGuiKey_6] = GLFW_KEY_6;
-		io.KeyMap[ImGuiKey_7] = GLFW_KEY_7;
-		io.KeyMap[ImGuiKey_8] = GLFW_KEY_8;
-		io.KeyMap[ImGuiKey_9] = GLFW_KEY_9;
-		io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-		io.KeyMap[ImGuiKey_B] = GLFW_KEY_B;
-		io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-		io.KeyMap[ImGuiKey_D] = GLFW_KEY_D;
-		io.KeyMap[ImGuiKey_E] = GLFW_KEY_E;
-		io.KeyMap[ImGuiKey_F] = GLFW_KEY_F;
-		io.KeyMap[ImGuiKey_G] = GLFW_KEY_G;
-		io.KeyMap[ImGuiKey_H] = GLFW_KEY_H;
-		io.KeyMap[ImGuiKey_I] = GLFW_KEY_I;
-		io.KeyMap[ImGuiKey_J] = GLFW_KEY_J;
-		io.KeyMap[ImGuiKey_K] = GLFW_KEY_K;
-		io.KeyMap[ImGuiKey_L] = GLFW_KEY_L;
-		io.KeyMap[ImGuiKey_M] = GLFW_KEY_M;
-		io.KeyMap[ImGuiKey_N] = GLFW_KEY_N;
-		io.KeyMap[ImGuiKey_O] = GLFW_KEY_O;
-		io.KeyMap[ImGuiKey_P] = GLFW_KEY_P;
-		io.KeyMap[ImGuiKey_Q] = GLFW_KEY_Q;
-		io.KeyMap[ImGuiKey_R] = GLFW_KEY_R;
-		io.KeyMap[ImGuiKey_S] = GLFW_KEY_S;
-		io.KeyMap[ImGuiKey_T] = GLFW_KEY_T;
-		io.KeyMap[ImGuiKey_U] = GLFW_KEY_U;
-		io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-		io.KeyMap[ImGuiKey_W] = GLFW_KEY_W;
-		io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-		io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-		io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+		
 
 
 		ImGui_ImplOpenGL3_Init("#version 410");
 
 	}
 
-	void ImGuiLayer::OnDettach()
+	void ImGuiLayer::OnDetach()
 	{
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
 	}
 
 	void ImGuiLayer::OnUpdate()
@@ -152,109 +72,22 @@ namespace Monstera {
 		float pos = io.DeltaTime;
 		// printf("delta time: (%.1f)\n", pos*1000);
 	}
-
-	void ImGuiLayer::OnEvent(Event& event)
+	void ImGuiLayer::Begin()
 	{
-		EventDispatcher dispatcher(event);
-		dispatcher.Dispatch<MouseButtonPressedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
-		dispatcher.Dispatch<MouseButtonReleasedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
-		dispatcher.Dispatch<MouseMovedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnMouseMovedEvent));
-		dispatcher.Dispatch<MouseScrolledEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnMouseScrolledEvent));
-		dispatcher.Dispatch<KeyPressedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
-		dispatcher.Dispatch<KeyReleasedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
-		dispatcher.Dispatch<KeyTypedEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
-		dispatcher.Dispatch<WindowResizeEvent>(MD_BIND_EVENT_FN(ImGuiLayer::OnWindowResizeEvent));
-
-
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 	}
-
-	bool ImGuiLayer::OnMouseButtonPressedEvent(MouseButtonPressedEvent& e)
+	void ImGuiLayer::End()
 	{
 		ImGuiIO& io = ImGui::GetIO();
-		io.MouseDown[e.getMouseButton()] = true;
+		Application& app = Application::Get();
+		io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
 
-		// printf("Mouse Position: (%.1f, %.1f)\n", (float)io.MousePos.x, (float)io.MousePos.y);
+		// Rendering
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		// We want other layers to handle this event
-		return false;
+		if (io.ConfigFlags & imguiconfigflags_Viewportsenable)
 	}
-
-	bool ImGuiLayer::OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.MouseDown[e.getMouseButton()] = false;
-
-
-		// io.MousePos = ImVec2(mouse_x, mouse_y); // Set your actual mouse coordinates
-		// Print mouse position
-		// printf("Mouse Position: (%.1f, %.1f)\n", io.MousePos.x, io.MousePos.y);
-
-		// We want other layers to receive this event
-		return false;
-	}
-
-	bool ImGuiLayer::OnMouseMovedEvent(MouseMovedEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.MousePos = ImVec2(e.GetX(), e.GetY());
-
-		//ImVec2 tempMousePos = io.MousePos.
-		// printf("Mouse Moved Event: (%.1f, %.1f)\n", io.MousePos.x, io.MousePos.y);
-		// We want other layers to receive this event
-		return false;
-	}
-
-	bool ImGuiLayer::OnMouseScrolledEvent(MouseScrolledEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.MouseWheelH += e.GetXOffset();
-		io.MouseWheel += e.GetYOffset();
-
-		// We want other layers to receive this event
-		return false;
-	}
-
-	bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.KeysDown[e.GetKeyCode()] = true;
-
-		io.KeyCtrl = io.KeysDown[GLFW_KEY_LEFT_CONTROL] || io.KeysDown[GLFW_KEY_RIGHT_CONTROL];
-		io.KeyShift = io.KeysDown[GLFW_KEY_LEFT_SHIFT] || io.KeysDown[GLFW_KEY_RIGHT_SHIFT];
-		io.KeyAlt = io.KeysDown[GLFW_KEY_LEFT_ALT] || io.KeysDown[GLFW_KEY_RIGHT_ALT];
-		io.KeySuper = io.KeysDown[GLFW_KEY_LEFT_SUPER] || io.KeysDown[GLFW_KEY_RIGHT_SUPER];
-
-		return false;
-	}
-
-	bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.KeysDown[e.GetKeyCode()] = false;
-
-		return false;
-	}
-
-	 bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent& e)
-	 {
-		 ImGuiIO& io = ImGui::GetIO();
-		 int keycode = e.GetKeyCode();
-		 if (keycode > 0 && keycode < 0x10000)
-			 io.AddInputCharacter((unsigned short)keycode);
-
-		return false;
-	 }
-
-	bool ImGuiLayer::OnWindowResizeEvent(WindowResizeEvent& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		io.DisplaySize = ImVec2((float)e.GetWidth(), (float)e.GetHeight());
-		io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
-		glViewport(0, 0, e.GetWidth(), e.GetHeight());
-
-		// printf("Window Size: (%.1f, %.1f)\n", (float)e.GetWidth(), (float)e.GetHeight());
-
-		return false;
-	}
-
 }
