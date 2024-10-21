724d967197eddd5b41fc8b123bf82ba7c8018bfb
MaxatHome <mhtwedt@gmail.com>
2024-10-20 21:15:00 -0500

    back to docking..

--- Monstera/src/Monstera/ImGui/ImGuiLayer.cpp
+++ Monstera/src/Monstera/ImGui/ImGuiLayer.cpp
@@ -116,7 +116,7 @@ namespace Monstera {
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		if (io.ConfigFlags) // & ImGuiConfigFlags_ViewportsEnable)
 		{
 			GLFWwindow* backup_current_context = glfwGetCurrentContext();
 			ImGui::UpdatePlatformWindows();
